233171ed38fe23e3245db6346d15b43184e2aa9d
Freek Van der Herten <freek@spatie.be>
2022-11-06 15:24:19 +0100

    wip

--- tests/IntegrationTest.php
+++ tests/IntegrationTest.php
@@ -7,7 +7,7 @@ use Symfony\Component\Process\Exception\ProcessTimedOutException;
 
 it('can get the scores of a real site', function () {
     $scores = Lighthouse::url('https://example.com')->run()->scores();
-dump($scores);
+
     // remove "performance" as that key is sometimes empty
     // on GitHub actions due to less powerful hardware
     $expectedCategories = Arr::without(Category::values(), Category::Performance->value);
